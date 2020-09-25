97d03e5f39fca2e496ed928732f954e8486d8ef0
Taras Senko <t.senko@viseven.com>
2020-09-25 21:19:12 +0300

    говно(

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,90 @@
 M.AutoInit();
 
 
+let store = {};
+let shop = document.querySelector('#cardShop')
 
-let product = document.querySelectorAll('.item'),
-    buttonPlus = document.querySelectorAll('.button-plus'),
-    buttonMinus = document.querySelectorAll('.button-minus'),
-    itemCounter = document.querySelectorAll('.item-counter');
-
-
-for (let i = 0; i < product.length; i++) {
-    itemCounter[i].textContent = itemCounter[i].dataset.count;
-    if (itemCounter[i].dataset.count == 5) {
-        buttonPlus[i].style.pointEvents = none;
-    } else {
-        buttonPlus[i].addEventListener('click', () => {
-            itemCounter[i].dataset.count = Number(itemCounter[i].dataset.count) + 1;
-            itemCounter[i].textContent = itemCounter[i].dataset.count;
-            return itemCounter[i].dataset.count
+    let client = contentful.createClient({
+        space: 'ffbcm6lpk46x',
+            environment: 'master', // defaults to 'master' if not set
+            accessToken: 'XW8MZTmiDrHY1hwJ7WkZ8zuRCuhmdKNkYLBa1la7xiM'
         })
-    }
+        client.getEntries()
+        .then(function (entries) {
+            let product = {};
+        entries.items.forEach(function (entry) {
+            if(entry.sys.contentType.sys.id == 'product') {
+                product[entry.fields.articul] = entry.fields;
+            }
+        })
+            store = product;
+            console.log(store)
+            shop.textContent = '';
+            for(let id in store){
+                let item = document.createElement('article');
+                id = store[id];
+                id.count = 0;
+                let img = id.image.fields;
+                item.className = 'item';
+                item.innerHTML = `
+                    <div class="item-header">
+                        <img src="${img.file.url}" alt="${img.title}">
+                        <h2>${id.name}</h2>
+                    </div>
+
+                    <p>${id.description}</p>
+                    <div class="price">
+                        <h3 class="item-price">price: <span><b>${id.price} UAH</b></span></h3>
+                        <div class="price__counter">
+                            <button class="button-minus" data-articul="${id.articul}">-</button>
+                            <p class="item-counter" data-count="${id.count}">${id.count}</p>
+                            <button class="button-plus" data-articul="${id.articul}">+</button>
+                        </div>
+                    </div>
+                `
+
+                shop.append(item);
+
+            }
+            return store;
+
+        });
+
+        document.addEventListener('DOMContentLoaded', function(){
+            let counter = document.querySelectorAll(".item-counter");
+            console.log(store)
+            for(let i in counter){
+                console.log(store)
+                console.log(counter[i])
+                console.log(counter)
+                counter[i].innerText = store[i].count;
+                //  = this.store.count
+            }
+
+            document.onclick = e => {
+                if(e.target.classList.contains('button-plus')){
+                    plusFunction(e.target.dataset.articul);
+                } else if(e.target.classList.contains('button-minus')){
+                    minusFunction(e.target.dataset.articul);
+                } else {
+                    return false;
+                }
+            }
+
+            const plusFunction = (id) => {
+                console.log(store[id].count)
+                if(store[id].count >= 0){
+                     store[id].count += 1
+                     console.log(store[id])
+                } else{
+                     store[id].count = 0
+                }
+            }
+
+        })
 
 
 
-}
+
+
+// document.querySelector('#cardShop').innerHTML = out;
